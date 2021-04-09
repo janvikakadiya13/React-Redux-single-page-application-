eefb151836b42c80425c2c22a9bb13ced594537c
janvikakadiya13 <70191967+janvikakadiya13@users.noreply.github.com>
2021-04-09 14:28:35 +0530

    Add files

---
+++ build/precache-manifest.fd9861312f99ea5d7fce2e5fabfcd4c7.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1eb94a792316a8741232bce56e604fe8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "94836ea1ba478fb871ab",
+    "url": "/static/css/3.0b3791e9.chunk.css"
+  },
+  {
+    "revision": "dbd8ff8e5137cb15afa3",
+    "url": "/static/css/4.216a2693.chunk.css"
+  },
+  {
+    "revision": "ed22424023dbbea8d8ce",
+    "url": "/static/css/5.f2b9a2ca.chunk.css"
+  },
+  {
+    "revision": "c0161d92ea214258e403",
+    "url": "/static/css/main.e2db34d2.chunk.css"
+  },
+  {
+    "revision": "883188eb4645feeacf51",
+    "url": "/static/js/2.a746fad0.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.a746fad0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "94836ea1ba478fb871ab",
+    "url": "/static/js/3.5ac3734d.chunk.js"
+  },
+  {
+    "revision": "dbd8ff8e5137cb15afa3",
+    "url": "/static/js/4.5700a896.chunk.js"
+  },
+  {
+    "revision": "ed22424023dbbea8d8ce",
+    "url": "/static/js/5.fd3d7684.chunk.js"
+  },
+  {
+    "revision": "c0161d92ea214258e403",
+    "url": "/static/js/main.fdca7904.chunk.js"
+  },
+  {
+    "revision": "5112bdf61d5f7f123b52",
+    "url": "/static/js/runtime-main.85af0270.js"
+  },
+  {
+    "revision": "b8503d262bffbfb7c67fd6762963e7d1",
+    "url": "/static/media/burgerLogo.b8503d26.png"
+  }
+]);
